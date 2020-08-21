0b022639c9c2330b6f4b7f20e3d8a155bf9fa459
Will Sieving <willsieving22@gmail.com>
2020-08-21 10:17:24 -0700

    Connect Past Events Page to Database

--- flask_server/routes.py
+++ flask_server/routes.py
@@ -1,7 +1,7 @@
 from flask import render_template, Blueprint, url_for, flash, redirect, request, abort
 from flask_server import db
 #from flask_server.posts.forms import PostForm
-from flask_server.models import Event
+from flask_server.models import Event, News
 from flask_login import current_user, login_required
 
 main = Blueprint('main', __name__)
@@ -19,11 +19,15 @@ def discussion():
 
 @main.route('/past_events')
 def past_events():
-    return render_template('past_events.html')
+    db.create_all()
+    events = Event.query.order_by(Event.date_posted.desc()).paginate(page=1, per_page=2)
+    return render_template('past_events.html', events=events)
 
-@main.route('/announcements')
-def announcements():
-    return render_template('announcements.html')
+@main.route('/news')
+def news():
+    db.create_all()
+    news = News.query.order_by(Event.date_posted.desc()).paginate(page=1, per_page=2)
+    return render_template('announcements.html', news=news)
 
 
 @main.route('/event/new', methods=['GET', 'POST'])
@@ -32,6 +36,20 @@ def new_event():
     event = Event(title='Test Event Title', content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
     # here we are getting the post's data from the forms and putting it in out previously created Post() model
     db.session.add(event)
+    # adding post to database
+    db.session.commit()
+
+
+
+    return redirect(url_for('main.home'))
+
+
+@main.route('/news/new', methods=['GET', 'POST'])
+def new_news():
+
+    news = News(title='Test Event Title', content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.')
+    # here we are getting the post's data from the forms and putting it in out previously created Post() model
+    db.session.add(news)
     # adding post to database
     db.session.commit()
 
